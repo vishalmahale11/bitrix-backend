9819347c9b50a258833915e06d9abaa18a4a1178
vishalmahle11 <vishalmahale974@gmail.com>
2022-09-28 20:52:22 +0530

    SignUp api schema is done and test it on postman

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./config/db");
+const loginRouter = require("./Components/Login/login.routes");
+const signupRouter = require("./Components/Signup/signup.routes");
+
+const PORT = 8080;
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
 
-app.listen(8080, () => {
-  console.log("http://localhost:8080");
+
+app.listen(PORT, async () => {
+  await dbConnect();
+  console.log(`http://localhost:${PORT}`);
 });
